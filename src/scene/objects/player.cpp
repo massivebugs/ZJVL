b625d6f73d29e98a03e21634d9e754ca880bcf1d
massivebugs <massivebugs@protonmail.com>
2021-10-04 01:58:51 +0900

    player cpp

---
+++ src/scene/objects/player.cpp
@@ -0,0 +1,50 @@
+#include "all.h"
+#include "player.h"
+#include "core/vec2.h"
+#include "scene/actor.h"
+#include "event/observer.h"
+#include "event/key_event.h"
+#include "event/mouse_event.h"
+
+namespace ZJVL
+{
+	Player::Player(const Vec2 &pos, const std::string &texture_path)
+		: Actor(pos, texture_path){};
+
+	bool Player::create(const Scene &scene) { return true; };
+	void Player::update(const Scene &scene, std::uint32_t dt){};
+	void Player::render(const Scene &scene, SDL_Renderer *renderer){};
+	void Player::destroy(){};
+
+	void Player::on_notify(Event &e)
+	{
+		switch (e.get_type())
+		{
+		case EventType::KEYDOWN:
+			switch (static_cast<KeyDownEvent &>(e).get_key())
+			{
+			case Key::ENTER:
+				break;
+			case Key::ESC:
+				break;
+			case Key::W:
+				pos.x += std::cos(angle) * 0.1;
+				pos.y += std::sin(angle) * 0.1;
+				break;
+			case Key::A:
+				pos.y += 0.1;
+				break;
+			case Key::S:
+				pos.x -= std::cos(angle) * 0.1;
+				pos.y -= std::sin(angle) * 0.1;
+				break;
+			case Key::D:
+				pos.x -= 0.1;
+				break;
+			}
+			break;
+		case EventType::MOUSEMOVE:
+			angle += static_cast<MouseMoveEvent &>(e).x * M_PI / 360;
+		}
+	}
+}
